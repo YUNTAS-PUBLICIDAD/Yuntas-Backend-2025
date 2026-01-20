a7f236c90ffb1d15a2c66bc3194cd02d1b26bbfc
Alexander <villafuertequispealex@gmail.com>
2026-01-19 23:37:36 -0500

    validacion fuente de lead

--- routes/api.php
+++ routes/api.php
@@ -65,7 +65,8 @@ Route::prefix('email')->group(function () {
     // Route::post('/mailing2', [App\Http\Controllers\Email\EmailController::class, 'enviarMailing2']);
     // Route::post('/mailing3', [App\Http\Controllers\Email\EmailController::class, 'enviarMailing3']);
 });
-//  RUTA PÚBLICA PARA EL POPUP
+
+// ------------------- LEADS (POPUP) -------------------
 Route::post('leads', [App\Http\Controllers\CRM\LeadController::class, 'store']);
 
 // ==============================================================================
@@ -85,7 +86,7 @@ Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
         Route::post('/{id}/role', [App\Http\Controllers\Admin\UserController::class, 'assignRole']);
     });
 
-    // ------------------- ADMINISTRACIÓN DE CATEGORÍAS -------------------
+    // ------------------- CATEGORÍAS -------------------
     Route::prefix('admin/categorias')->group(function () {
         Route::post('/', [App\Http\Controllers\Admin\Category\CategoryController::class, 'store']);
         Route::put('/{id}', [App\Http\Controllers\Admin\Category\CategoryController::class, 'update']);
@@ -107,7 +108,7 @@ Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
         Route::delete('/{id}', [App\Http\Controllers\Support\ContactMessageController::class, 'destroy']);
     });
 
-    // ------------------- CRM / LEADS -------------------
+    // ------------------- LEADS -------------------
     Route::prefix('admin/leads')->group(function () {
         Route::post('/', [App\Http\Controllers\CRM\LeadController::class, 'store']);
         Route::get('/', [App\Http\Controllers\CRM\LeadController::class, 'index']);  
