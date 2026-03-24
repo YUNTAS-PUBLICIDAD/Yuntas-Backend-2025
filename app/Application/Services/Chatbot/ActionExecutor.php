b44a115ba2f79ef3e18bbf2681661e63bed8d6a7
Bryan Mancilla <bmancilla119@gmail.com>
2026-03-23 19:17:29 -0500

    feat; apllcar manejo centralizado de N8N

--- app/Application/Services/Chatbot/ActionExecutor.php
+++ app/Application/Services/Chatbot/ActionExecutor.php
@@ -7,6 +7,7 @@ use Log;
 
 class ActionExecutor
 {
+  // Ejecuta SOLO acciones ya filtradas
   public function execute($actions, $conversation, $message)
   {
     foreach ($actions as $action) {
@@ -17,18 +18,14 @@ class ActionExecutor
         // 'call_n8n' => $this->callN8N($action, $conversation),
         'update_context'=> $this->updateContext($action, $conversation),
         'log' => $this->logAction($action, $conversation),
-        'call_n8n' => $this->skipN8N($action),
+        // 'call_n8n' => $this->skipN8N($action),
+        'call_n8n' => $this->handleN8N($action, $conversation),
          default => null
       };
     }
   }
 
-  protected function shouldRun($action, $conversation, $message)
-  {
-    return app(ConditionEvaluator::class)
-    ->evaluate($action, $conversation, $message);
-  }
-
+  // Filtra acciones que cumplen condiciones
   public function filterExecutable($actions, $conversation, $message)
   {
     return collect($actions)->filter(function ($action) use ($conversation, $message){
@@ -36,36 +33,69 @@ class ActionExecutor
     });
   }
 
+  // Evalúa condiciones
+  protected function shouldRun($action, $conversation, $message)
+  {
+    return app(ConditionEvaluator::class)
+    ->evaluate($action, $conversation, $message);
+  }
+
+  // Manejo centralizado de N8N
+  protected function handleN8N($action, $conversation)
+  {
+    if (config('chatbot.n8n_enabled')) {
+      $this->callN8N($action, $conversation);
+    }else {
+      $this->skipN8N($action);
+    }
+  }
+
+  // Llamanda real a N8N
   protected function callN8N($action, $conversation)
   {
-    $params = $action->parameters;
+    // $params = $action->parameters;
+    $params = $action->parameters ?? [];
+    if (!isset($params['webhook_url'])) {
+      return;
+    }
     Http::post($params['webhook_url'], [
       'conversation_id' => $conversation->id,
       'context' => $conversation->context
     ]);
   }
 
-  protected function logAction($action, $conversation)
-  {
-      Log::info('Chatbot action executed', [
-        'action_id' => $action->id,
-        'conversation_id' => $conversation->id
-      ]);
-  }
-
+  
+  // Simulación (cuando N8N está desactivado)
   protected function skipN8N($action)
   {
     Log::info('N8N action skipped', [
       'action_id' => $action->id,
       'name' => $action->name
-    ]);
-  }
+      ]);
+      }
 
+      // Loggin de acciones
+      protected function logAction($action, $conversation)
+      {
+          Log::info('Chatbot action executed', [
+            'action_id' => $action->id,
+            'conversation_id' => $conversation->id
+          ]);
+      }
+
+  // Actualiza contexto de forma segura
   protected function updateContext($action, $conversation)
   {
-    $params = $action->parameters;
+    // $params = $action->parameters;
+    $params = $action->parameters ?? [];
+    if (!isset($params['key'], $params['value'])) {
+    return;
+    }
     $context = $conversation->context ?? [];
-    $context[$params['key']] = $params['value'];
+    // $context[$params['key']] = $params['value'];
+    // Permite Keys tipo: context.user.name
+    data_set($context, $params['key'], $params['value']);
+
     $conversation->update([
       'context' => $context
     ]);
