8311d6ac44dbf3eaa187aef47a7c5a4e4779375e
Bryan Mancilla <bmancilla119@gmail.com>
2026-03-24 18:50:59 -0500

    refactor: optimizar logica para extraer nombre

--- app/Application/Services/Chatbot/MessageParser.php
+++ app/Application/Services/Chatbot/MessageParser.php
@@ -6,32 +6,13 @@ class MessageParser
 {
   public function extractName(string $message): ?string
   {
-    // $message = strtolower($message);
-    //  if (preg_match('/(me llamo|soy|mi nombre es)\s+([a-zA-Z]+)/', $message, $matches)) {
-    //         return ucfirst($matches[2]);
-    //     }
+    $message = trim($message);
 
-    //     return null;
-    // Mantener original para formato final
-    $original = $message;
+    if (preg_match('/\b(me llamo|soy|mi nombre es)\b\s+([a-zA-ZáéíóúÁÉÍÓÚñÑ]{2,})/i', $message, $matches)) {
+        return $this->formatName($matches[2]);
+    }
 
-    // Normalizar para detectar patrón
-    $normalized = strtolower($message);
-    
-     if (preg_match('/(me llamo|soy|mi nombre es)\s+(.+)/i', $normalized, $matches)) {
-
-            $namePart = $matches[2];
-
-            // Limpiar basura: emojis, símbolos raros
-            $namePart = preg_replace('/[^a-zA-ZáéíóúÁÉÍÓÚñÑ\s]/u', '', $namePart);
-
-            // Tomar máximo 2 palabras (nombre + apellido)
-            $words = array_slice(explode(' ', trim($namePart)), 0, 2);
-
-            $name = implode(' ', $words);
-
-            return ucwords($name);
-        }
-        return null;
+    return null;
   }
+  private function formatName(string $name): string { return ucwords(strtolower($name)); }
 }
