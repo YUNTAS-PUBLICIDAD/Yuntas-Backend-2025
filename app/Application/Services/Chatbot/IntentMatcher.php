bf16fd601418712c4acd820ad8dbb82adeadab8d
Bryan Mancilla <bmancilla119@gmail.com>
2026-03-25 00:17:40 -0500

    feat: agregar soporte para coincidencias y relevancia

--- app/Application/Services/Chatbot/IntentMatcher.php
+++ app/Application/Services/Chatbot/IntentMatcher.php
@@ -10,19 +10,43 @@ class IntentMatcher
   {
     $message = strtolower($message);
 
-    return ChatbotIntent::with('questions')
-    ->get()
-    ->first(function ($intent) use ($message) {
-      return $intent->questions->contains(function ($q) use ($message){
+    $intents = ChatbotIntent::with('questions')->get();
+
+    $bestIntent = null;
+    $bestScore = 0;
+
+    foreach ($intents as $intent) {
+      $score = 0;
+
+      foreach ($intent->questions as $q) {
         $keywords = $q->keywords ?? [];
 
         foreach ($keywords as $keyword) {
           if (str_contains($message, strtolower($keyword))) {
-            return true;
+            $score++;
           }
         }
-        return false;
-      });
-    });
+      }
+      if ($score > $bestScore) {
+        $bestScore = $score;
+        $bestIntent = $intent;
+      }
+    }
+    return $bestScore > 0 ? $bestIntent : null;
+
+    // return ChatbotIntent::with('questions')
+    // ->get()
+    // ->first(function ($intent) use ($message) {
+    //   return $intent->questions->contains(function ($q) use ($message){
+    //     $keywords = $q->keywords ?? [];
+
+    //     foreach ($keywords as $keyword) {
+    //       if (str_contains($message, strtolower($keyword))) {
+    //         return true;
+    //       }
+    //     }
+    //     return false;
+    //   });
+    // });
   }
 }
