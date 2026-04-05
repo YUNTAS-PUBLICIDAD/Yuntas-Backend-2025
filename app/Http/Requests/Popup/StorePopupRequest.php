4173831d984d5865c8e069a8d13dd46545651d92
Bryan Mancilla <bmancilla119@gmail.com>
2026-04-04 21:08:42 -0500

    refactor: limpiar comentarios innecesarios

--- app/Http/Requests/Popup/StorePopupRequest.php
+++ app/Http/Requests/Popup/StorePopupRequest.php
@@ -41,10 +41,6 @@ class StorePopupRequest extends FormRequest
       'images' => ['required', 'array', 'size:3'],
       'images.*' => ['required', 'array'],
       'images.*.file' => 'required|image|mimes:jpg,jpeg,png,webp|max:2048',
-      // 'images.*.file' => [
-      //   'required', 'file', 'mimes:jpg,png,webp',
-      //   'max:2048'
-      // ],
      'images.*.device' => ['required', Rule::in(['desktop', 'mobile'])],
      'images.*.slot' => ['required', Rule::in(['left', 'right', 'center'])],
      'images.*.alt' => 'nullable|string|max:255',
