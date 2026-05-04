498e6e2a3070081c60cbbdbeedf2959261ba28d6
Bryan Mancilla <bmancilla119@gmail.com>
2026-05-03 21:03:33 -0500

    feat: actualizar modelo template para variantes y assets

--- app/Models/Template.php
+++ app/Models/Template.php
@@ -7,27 +7,32 @@ use Illuminate\Database\Eloquent\Model;
 class Template extends Model
 {
 protected $fillable = [
-  'lead_source_id',
+  // 'lead_source_id',
   'name',
   'active',
 ];
 
-public function leadSource()
-{
-  return $this->belongsTo(LeadSource::class);
-}
+// public function leadSource()
+// {
+//   return $this->belongsTo(LeadSource::class);
+// }
 
-public function contents()
-{
-  return $this->hasMany(TemplateContent::class);
-}
+// public function contents()
+// {
+//   return $this->hasMany(TemplateContent::class);
+// }
 
 // Helper útil
-public function getContentByChannel($channel)
+// public function getContentByChannel($channel)
+// {
+//   return $this->contents()
+//   ->where('channel', $channel)
+//   ->where('active', true)
+//   ->first();
+// }
+
+public function variants()
 {
-  return $this->contents()
-  ->where('channel', $channel)
-  ->where('active', true)
-  ->first();
+  return $this->hasMany(TemplateVariant::class);
 }
 }
