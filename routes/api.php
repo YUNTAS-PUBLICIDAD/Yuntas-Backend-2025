26c4b715b0f2060bcb881c9f4fbddd96a0a39e86
Bryan Mancilla <bmancilla119@gmail.com>
2026-05-09 09:51:37 -0500

    feat: agregar nueva ruta para admin categorias

--- routes/api.php
+++ routes/api.php
@@ -121,6 +121,7 @@ Route::middleware(['auth:sanctum', 'role:admin|marketing|ventas', 'throttle:admi
 
   // ------------------- CATEGORÍAS -------------------
   Route::prefix('admin/categorias')->group(function () {
+    Route::get('/', [CategoryController::class, 'index']);
     Route::post('/', [CategoryController::class, 'store']);
     Route::put('/{id}', [CategoryController::class, 'update']);
     Route::delete('/{id}', [CategoryController::class, 'destroy']);
