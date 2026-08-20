ce72d0c1587b93546a189b2651c904f50c46e1c1
ErickMendoza21 <erick.mendoza@upeu.edu.pe>
2026-08-20 10:36:03 -0500

    fix: corregir detección real de IP

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        
+        $middleware->trustProxies(at: '*');
+
         $middleware->append(\App\Http\Middleware\SanitizeInput::class);
         $middleware->append(\App\Http\Middleware\RemoveHeaders::class);
 
